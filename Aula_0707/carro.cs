cf9a65d09f59fa0727bf9fa1afedd1bddbc57e0b
karinalima <cinderelakarina@hotmail.com>
2015-08-03 13:29:41 -0300

    car

---
+++ Aula_0707/carro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_0707
+{
+    class Carro
+    {
+        public string placa;
+        public string modelo;
+        public double consumoKmL;
+        public double qtdCombustivelL;
+
+        public double AutonomiaKm()
+        {
+            double autonomia = consumoKmL * qtdCombustivelL;
+            return autonomia;
+        }
+    }
+}
