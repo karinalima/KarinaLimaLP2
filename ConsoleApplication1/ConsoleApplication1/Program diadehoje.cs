548bfc52ad614e067e3f2824d886e1fb4e8c179e
KarinaLima <cinderelakarina@hotmail.com>
2015-08-11 17:50:09 -0300

    Aula1108

---
+++ ConsoleApplication1/ConsoleApplication1/Program diadehoje.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataNasc = new DateTime();
+            alo t = new alo("ALO ALOOO");          //criando um objeto da classe alo
+
+            string k = "karina";
+            Console.WriteLine(k.Substring(2, 4));
+        }
+    }
+}
