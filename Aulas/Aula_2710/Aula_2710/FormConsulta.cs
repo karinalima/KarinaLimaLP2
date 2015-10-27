b8c956653dcc4ff22de1433bdca3eb472da4d092
Karina Lima <cinderelakarina@hotmail.com>
2015-10-27 17:57:22 -0300

    Aula_2710

---
+++ Aulas/Aula_2710/Aula_2710/FormConsulta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Aula_2710
+{
+    public partial class FormConsulta : Form
+    {
+        public FormConsulta()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            MySqlDataReader reader = Command.ExecuteReader();
+
+            if (reader.HasRows)
+                
+            {
+                while (reader.Read())
+                {
+                     
+                }
+            }
+        
+        }
+    }
+}
---
+++ Aulas/Aula_2710/Aula_2710/FormPrincipal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula_2710
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormConsulta consulta = new FormConsulta();
+            consulta.ShowDialog();
+
+        }
+    }
+}
