5c4f06759332f697662bace560892e4a5bcf6cb3
karinalima <cinderelakarina@hotmail.com>
2015-11-19 16:49:02 -0200

    Ultimo, exercicio 14

---
+++ Exercícios/Exercicio_14/Exercicio_14/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.SqlClient;
+
+namespace Exercicio_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario user = new Usuario();
+            
+            string query = "INSERT INTO USUARIO (Login, Senha) VALUES (@login, @senha)";
+            MySqlConnection conn = new MySqlConnection("Server=localhost;Database=test;Uid=root;Pwd=");
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = conn;
+            
+
+            bool logar = false;
+
+            while (logar == false)
+            {
+                conn.Open();
+                Console.WriteLine("Digite seu login: ");
+                string login = Console.ReadLine();
+                Console.WriteLine("Digite sua senha: ");
+                string senha = Console.ReadLine();
+                string query = "INSERT INTO USUARIO (Login, Senha) VALUES (@login, @senha)";
+                cmd.Parameters.AddWithValue("@login", login);
+                cmd.Parameters.AddWithValue("@senha", senha);
+                cmd.commandText = query;
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+
+                if (reader.HasRows)
+                {
+                    while (reader.read())
+                    {
+
+                        user.Id = reader.GetInt32(0);
+                        user.Nome = reader.GetString(1);
+                        user.Dataultimoacesso = DateTime.Now;
+                        logar = true;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine(" O Login ou Senha estão incorreto");
+                }
+                if (logar == true)
+                {
+                    reader.Close();
+                    Console.WriteLine("Login realizado com sucesso!");
+                    cmd.CommandText = String.Format("INSERT INTO USUARIO (Dataultimoacesso) VALUES ('{0}')");
+                    cmd.Connection.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+
+
+                }
+                else
+                    Console.WriteLine("Falha no login");
+               
+            } 
+         
+          
+        }
+    }
+}
+
---
+++ Exercícios/Exercicio_14/Exercicio_14/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Exercicio_14
+{
+    class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public DateTime Dataultimoacesso { get; set; }
+    }
+}
